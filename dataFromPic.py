6f47f7f94d24b40a2488f564b1fda302c72b1681
Norbert <bnorbert88@gmail.com>
2019-11-14 14:08:20 +0100

    Minimal modification

--- dataFromPic.py
+++ dataFromPic.py
@@ -1,8 +1,8 @@
 from PIL import Image
-import numpy as np
+from numpy import asarray
 
 i = Image.open('images/image4995.png')
-data = np.asarray(i)
+data = asarray(i)
 
 with open('images/image4995.txt', 'w') as outfile:
     for row in data:
@@ -12,19 +12,3 @@ with open('images/image4995.txt', 'w') as outfile:
             rgb = '['+','.join(map(str, pixel))+']\n'
             outfile.write(rgb)
         outfile.write('\n')
-
-    # # I'm writing a header here just for the sake of readability
-    # # Any line starting with "#" will be ignored by numpy.loadtxt
-    # outfile.write('# Array shape: {0}\n'.format(data.shape))
-    #
-    # # Iterating through a ndimensional array produces slices along
-    # # the last axis. This is equivalent to data[i,:,:] in this case
-    # for data_slice in data:
-    #     outfile.write('# New row\n')
-    #     # The formatting string indicates that I'm writing out
-    #     # the values in left-justified columns 7 characters in width
-    #     # with 2 decimal places.
-    #     np.savetxt(outfile, data_slice, fmt=repr(['%d', '%d', '%d', '%d']), newline='\n')
-    #
-    #     # Writing out a break to indicate different slices...
-    #     outfile.write('\n')
